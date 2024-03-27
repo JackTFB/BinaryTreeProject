cd0a4c3eecd08b3bd12c0eeca162690b0b715978
JackTFB <jacktfbofficial@gmail.com>
2024-03-27 01:39:57 -0400

    Implemented the binary tree, and created a testing mechanism for the tree that sorts the numbers

--- btnode_int.java
+++ btnode_int.java
@@ -8,43 +8,58 @@ public class btnode_int
     // constructor
     public btnode_int(int initialData)
     {
-
+        data = initialData;
+        left = null;
+        right = null;
     } 
     
     // Gets the data from this node
     public int getData( )
     {
-
+        return data;
     } 
 
     // Gets reference to left child
     public btnode_int getLeft( )
     {
-
+        return left;
     } 
 
     // Gets reference to right child
     public btnode_int getRight( )
     {
-
+        return right;
     } 
     
     // Set data of this node
     public void setData(int newData) 
     {
-
+        data = newData;
     }
 
     // Set left child
     public void setLeft(btnode_int newLeft) 
     {
-
+        left = newLeft;
     }
 
     // Set right child
     public void setRight(btnode_int newRight) 
     {
+        right = newRight;
+    }
 
+    public void InorderPrint()
+    {
+        if (left != null)
+        {
+            left.InorderPrint();
+        }
+        System.out.println(data);
+        if (right != null)
+        {
+            right.InorderPrint();
+        }
     }
 
 
--- tree_tester.java
+++ tree_tester.java
@@ -1,7 +1,22 @@
 public class tree_tester 
 {
     public static void main (String[] args)
-    {
-        
+    {  
+        TestTree();
     }
+
+    public static void TestTree ()
+    {
+        btnode_int root = new btnode_int(5);
+        root.setLeft(new btnode_int(3));
+        root.getLeft().setLeft(new btnode_int(1));
+        root.getLeft().setRight(new btnode_int(4));
+        root.setRight(new btnode_int(9));
+        root.getRight().setLeft(new btnode_int(6));
+
+        root.InorderPrint();
+
+    }
+
+    
 }
